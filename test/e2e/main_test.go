0f1e14e372d9067ac78bb618140498ea835ca7b6
Jonathan Ogilvie <679297+jcogilvie@users.noreply.github.com>
2025-10-24 17:14:30 -0400

    Speed up e2es (#105)
    * Converge `main` tests on same version of function gotpl
    
    Signed-off-by: Jonathan Ogilvie <jonathan.ogilvie@sumologic.com>
    
    * Speed up e2es:  set up cluster providers once at beginning of suite
    
    Signed-off-by: Jonathan Ogilvie <jonathan.ogilvie@sumologic.com>
    
    * Fix lint
    
    Signed-off-by: Jonathan Ogilvie <jonathan.ogilvie@sumologic.com>
    
    ---------
    
    Signed-off-by: Jonathan Ogilvie <jonathan.ogilvie@sumologic.com>

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -19,14 +19,23 @@ package e2e
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/e2e-framework/klient/conf"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
@@ -34,6 +43,7 @@ import (
 	"sigs.k8s.io/e2e-framework/support/kind"
 	"sigs.k8s.io/e2e-framework/third_party/helm"
 
+	pkgv1 "github.com/crossplane/crossplane/v2/apis/pkg/v1"
 	"github.com/crossplane/crossplane/v2/test/e2e/config"
 	"github.com/crossplane/crossplane/v2/test/e2e/funcs"
 )
@@ -132,6 +142,95 @@ func TestMain(m *testing.M) {
 
 	// We always want to add our types to the scheme.
 	setup = append(setup, funcs.AddCrossplaneTypesToScheme(), funcs.AddCRDsToScheme())
+
+	// Install shared functions and providers for all tests in this variant
+	sharedSetupPath := filepath.Join("test/e2e/manifests/beta/diff", imageTag, "_setup")
+
+	setup = append(setup, func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
+		client, err := cfg.NewClient()
+		if err != nil {
+			return ctx, fmt.Errorf("failed to create k8s client: %w", err)
+		}
+
+		// Read and apply all YAML files in the setup directory
+		files, err := filepath.Glob(filepath.Join(sharedSetupPath, "*.yaml"))
+		if err != nil {
+			return ctx, fmt.Errorf("failed to glob setup files: %w", err)
+		}
+
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				return ctx, fmt.Errorf("failed to open %s: %w", file, err)
+			}
+
+			decoder := yaml.NewYAMLOrJSONDecoder(f, 4096)
+
+			for {
+				obj := &unstructured.Unstructured{}
+				if err := decoder.Decode(obj); err != nil {
+					if errors.Is(err, io.EOF) {
+						break
+					}
+
+					f.Close()
+
+					return ctx, fmt.Errorf("failed to decode %s: %w", file, err)
+				}
+
+				if err := client.Resources().Create(ctx, obj); err != nil {
+					f.Close()
+					return ctx, fmt.Errorf("failed to create resource from %s: %w", file, err)
+				}
+			}
+
+			f.Close()
+		}
+
+		// Wait for functions to be ready
+		functionList := &pkgv1.FunctionList{}
+		if err := wait.For(conditions.New(client.Resources()).ResourcesFound(functionList), wait.WithTimeout(30*time.Second)); err != nil {
+			return ctx, fmt.Errorf("functions not found: %w", err)
+		}
+
+		if err := client.Resources().List(ctx, functionList); err != nil {
+			return ctx, fmt.Errorf("failed to list functions: %w", err)
+		}
+
+		for _, fn := range functionList.Items {
+			obj := fn.DeepCopy()
+			if err := wait.For(conditions.New(client.Resources()).ResourceMatch(obj, func(object k8s.Object) bool {
+				fn := object.(*pkgv1.Function)
+				return fn.Status.GetCondition(pkgv1.TypeHealthy).Status == "True" &&
+					fn.Status.GetCondition(pkgv1.TypeInstalled).Status == "True"
+			}), wait.WithTimeout(3*time.Minute)); err != nil {
+				return ctx, fmt.Errorf("function %s not ready: %w", fn.Name, err)
+			}
+		}
+
+		// Wait for provider to be ready
+		providerList := &pkgv1.ProviderList{}
+		if err := wait.For(conditions.New(client.Resources()).ResourcesFound(providerList), wait.WithTimeout(30*time.Second)); err != nil {
+			return ctx, fmt.Errorf("providers not found: %w", err)
+		}
+
+		if err := client.Resources().List(ctx, providerList); err != nil {
+			return ctx, fmt.Errorf("failed to list providers: %w", err)
+		}
+
+		for _, prov := range providerList.Items {
+			obj := prov.DeepCopy()
+			if err := wait.For(conditions.New(client.Resources()).ResourceMatch(obj, func(object k8s.Object) bool {
+				prov := object.(*pkgv1.Provider)
+				return prov.Status.GetCondition(pkgv1.TypeHealthy).Status == "True" &&
+					prov.Status.GetCondition(pkgv1.TypeInstalled).Status == "True"
+			}), wait.WithTimeout(2*time.Minute)); err != nil {
+				return ctx, fmt.Errorf("provider %s not ready: %w", prov.Name, err)
+			}
+		}
+
+		return ctx, nil
+	})
 
 	if environment.ShouldCollectKindLogsOnFailure() {
 		finish = append(finish, envfuncs.ExportClusterLogs(environment.GetKindClusterName(), environment.GetKindClusterLogsLocation()))
